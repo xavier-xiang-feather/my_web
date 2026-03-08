1aa3f43a3b92a94ed624c63a141f2d890b3f1fa2
xijian xiang <x4xiang@ucsd.edu>
2026-03-08 14:08:17 -0700

    test auth

--- reporting_site/includes/config.php
+++ reporting_site/includes/config.php
@@ -3,5 +3,5 @@ session_start();
 
 define('APP_USERNAME', 'grader');
 
-define('APP_PASSWORD_HASH', '$2y$10$s4pYZaXpP6E3MEOSpcieC.mvTdRuToSW4rVRVfThi4bZbLK.XoeV2');
+define('APP_PASSWORD_HASH', 'cse135');
 ?>
