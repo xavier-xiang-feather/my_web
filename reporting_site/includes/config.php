ce7aa137da0dc0fa4c41cfbf754befd1807e627e
xijian xiang <x4xiang@ucsd.edu>
2026-03-08 14:05:19 -0700

    finish part1?

--- reporting_site/includes/config.php
+++ reporting_site/includes/config.php
@@ -3,5 +3,5 @@ session_start();
 
 define('APP_USERNAME', 'grader');
 
-define('APP_PASSWORD', password_hash('cse135', PASSWORD_DEFAULT));
+define('APP_PASSWORD_HASH', '$2y$10$s4pYZaXpP6E3MEOSpcieC.mvTdRuToSW4rVRVfThi4bZbLK.XoeV2');
 ?>
